386158cd4c4fc82ff0243e8ed24362b19d50feee
sarthak shree <157057589+sarthak-shree@users.noreply.github.com>
2026-03-08 11:28:59 +0530

    Added my first Python programs

---
+++ Create a Personalized Greeting.py
@@ -0,0 +1,6 @@
+x = input("Enter your first name:  ")
+y = input("Enter your last name:  ")
+
+print(' ')
+
+print("Hello, " + x + " " + y + "! Welcome to Python programming.")
---
+++ Perform Basic Mathematical Operations.py
@@ -0,0 +1,15 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+#  basic mathematical operations on these two numbers:
+addition = (num1) + (num2)
+subtraction = (num1) - (num2)
+multiplication = (num1) * (num2)
+division = (num1) / (num2)
+
+print('') 
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)  
+print("Multiplication:", multiplication)
+print("Division:", division)
